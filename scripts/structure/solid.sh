53fb2976d4a0f7ebfdc6cb12f65764dc7f953573
jihad-hossain1 <jihadkhan934@gmail.com>
2026-02-02 10:58:52 +0600

    separate js boiler plate

--- scripts/structure/solid.sh
+++
@@ -1,7 +0,0 @@
-echo "📂 Generating SOLID Layered Structure..."
-mkdir -p src/{controllers,services,repositories,models,routes,dtos,utils,middlewares}
-
-# Layered files
-touch src/services/.gitkeep
-touch src/repositories/.gitkeep
-
